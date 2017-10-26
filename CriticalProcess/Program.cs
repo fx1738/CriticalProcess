51d621dbd7b66f14e612b2cc5f93ad079a972ba3
Fx <fexile@outlook.com>
2017-10-26 11:00:28 +0200

    eadf

--- CriticalProcess/Program.cs
+++ CriticalProcess/Program.cs
@@ -22,7 +22,7 @@ namespace CriticalProcess
             Process.EnterDebugMode();
             NtSet(1, 0x1D);
             // do your stuff here
-            Console.WriteLine("Nice Process");
+            Console.WriteLine("Nice Proaaacess");
             Console.ReadLine();
 
             // end critical mode
